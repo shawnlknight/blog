e4360004a6affc2bda7fe06f3ded6265819c2fda
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-10-18 11:00:48 -0400

    js

---
+++ main.js
@@ -0,0 +1,13 @@
+// js for site
+
+// jQuery for drop down menu on mobile. Once incorporated, remove hover effect from .trigger in css
+$(document).ready(function() {
+	$(".menu-icon").click(function() {
+		$(".trigger").toggleClass("hide");
+	});
+
+	if ($(".mainNav").is('visible')) {
+		$(".trigger").addClass("hide");
+	}
+});
+
