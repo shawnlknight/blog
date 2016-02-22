f0b1b839dc7e1bf4af52a757ee8d319ba319326c
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2016-02-21 20:23:35 -0500

    adding slowToggle for dropdown

--- _layouts/main.js
+++ _layouts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$(".menu-icon").click(function() {
-		$(".trigger").toggleClass("hide").slideToggle('slow');
+		$(".trigger").toggleClass("hide");
 		console.log("success");
 	})
 });
